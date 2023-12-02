eb4fe10e46f9a71d2fe3119002fa6c28fd1128e4
JacobyIA <148162932+JacobyIA@users.noreply.github.com>
2023-12-02 14:53:50 -0500

    Disabled limit switches on ElevatorSubsystem.java
    Just commented them out, will reenable once ready with changes.

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -11,8 +11,8 @@ import edu.wpi.first.wpilibj.DigitalInput;
 public class ElevatorSubsystem extends SubsystemBase{
 
     private CANSparkMax liftMotor;
-    private DigitalInput toplimitSwitch = new DigitalInput(ElevatorConstants.ELEVATOR_TOP_SWITCH_CHANNEL);
-    private DigitalInput bottomlimitSwitch = new DigitalInput(ElevatorConstants.ELEVATOR_BOTTOM_SWITCH_CHANNEL);
+    // private DigitalInput toplimitSwitch = new DigitalInput(ElevatorConstants.ELEVATOR_TOP_SWITCH_CHANNEL);
+    // private DigitalInput bottomlimitSwitch = new DigitalInput(ElevatorConstants.ELEVATOR_BOTTOM_SWITCH_CHANNEL);
     
 
     public ElevatorSubsystem(){
@@ -29,12 +29,14 @@ public class ElevatorSubsystem extends SubsystemBase{
     }
 
     public void up(){
-        if(!toplimitSwitch.get()) set(ElevatorConstants.ELEVATOR_UP_SPEED);
-        else set(0);
+        // if(!toplimitSwitch.get()) set(ElevatorConstants.ELEVATOR_UP_SPEED);
+        // else set(0);
+        set(ElevatorConstants.ELEVATOR_UP_SPEED);
     }
 
     public void down(){
-        if(!bottomlimitSwitch.get()) set(ElevatorConstants.ELEVATOR_DOWN_SPEED);
-        else set(0);
+        // if(!bottomlimitSwitch.get()) set(ElevatorConstants.ELEVATOR_DOWN_SPEED);
+        // else set(0);
+        set(ElevatorConstants.ELEVATOR_DOWN_SPEED);
     }
 }
